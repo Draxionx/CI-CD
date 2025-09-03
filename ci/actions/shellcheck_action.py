beff0893369d7daef723f6139faae91ea140979a
dylanw11 <dylanw11@students.cs.ubc.ca>
2025-09-03 11:14:01 -0700

    aditional fixes

--- ci/actions/shellcheck_action.py
+++ ci/actions/shellcheck_action.py
@@ -9,8 +9,7 @@ class ShellcheckAction(BaseAction):
     def command(self) -> str:
         globs = os.getenv("SH_GLOBS", "**/*.sh")
         args  = os.getenv("SHELLCHECK_ARGS", "-S style -x")
-        # Use bash -lc so we can do a safe file list + conditional skip
-        # Note: base.run() invokes with shell=True, so this full string is fine.
+
         return (
             "bash -lc \""
             f"files=\\\"$(git ls-files {globs} || true)\\\"; "
